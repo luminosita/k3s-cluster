669201067db9177a33b8b19378eb909710b5a363
Semaphore <sem@emisia.net>
2022-12-26 03:10:50 +0100

    Changed strategy. Host IPs will be fed to Vagrant for provider and provisioner to fix IP address

--- infra/vagrant/scripts/bootstrap.sh
+++ infra/vagrant/scripts/bootstrap.sh
@@ -1,15 +1,15 @@
-#echo "SSH Host"
-#echo $SSH_HOST
+echo "SSH Host"
+echo $SSH_HOST
 
 echo 'Copying host public SSH Keys to the VM'
 mkdir -p /home/vagrant/.ssh
 chmod 700 /home/vagrant/.ssh
 cat /home/vagrant/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
 chmod -R 600 /home/vagrant/.ssh/authorized_keys
-#echo "Host $SSH_HOST" >> /home/vagrant/.ssh/config
-#echo 'StrictHostKeyChecking no' >> /home/vagrant/.ssh/config
-#echo 'UserKnownHostsFile /dev/null' >> /home/vagrant/.ssh/config
-#chmod -R 600 /home/vagrant/.ssh/config
+echo "Host $SSH_HOST" >> /home/vagrant/.ssh/config
+echo 'StrictHostKeyChecking no' >> /home/vagrant/.ssh/config
+echo 'UserKnownHostsFile /dev/null' >> /home/vagrant/.ssh/config
+chmod -R 600 /home/vagrant/.ssh/config
 rm /home/vagrant/id_rsa.pub
 
 echo 'Recreating OpenSSH keys'
